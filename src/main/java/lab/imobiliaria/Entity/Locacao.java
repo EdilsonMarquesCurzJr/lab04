316fa0ca8a96ce98c42e744a495ad08a3a188f89
Edilson Marques <104401369+EdilsonMarquesCurzJr@users.noreply.github.com>
2024-07-29 22:05:05 -0300

    8

--- src/main/java/lab/imobiliaria/Entity/Locacao.java
+++ src/main/java/lab/imobiliaria/Entity/Locacao.java
@@ -18,7 +18,7 @@ public @Data class Locacao {
     private Integer id;
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "ID_IMOVEL")
-    private Imoveis idImovel;
+    private Imoveis imovel;
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "ID_INQUILINO")
     private Cliente idInquilino;
@@ -42,7 +42,6 @@ public @Data class Locacao {
     public String toString() {
         return "Locacao{" +
                 "id=" + id +
-                ", idImovel=" + idImovel +
                 ", idInquilino=" + idInquilino +
                 ", valorAluguel=" + valorAluguel +
                 ", pecentualMulta=" + pecentualMulta +
